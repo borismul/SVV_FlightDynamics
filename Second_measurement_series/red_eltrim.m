8b656645232ab992f366a6f660ecb61bd6bbb2b1
Karl Martin Kajak <karlmartinkajak@wlan-145-94-204-223.wlan.tudelft.nl>
2015-03-10 16:39:06 +0100

    Created the calculation for reduced elevator trim deflection.

---
+++ Second_measurement_series/red_eltrim.m
@@ -0,0 +1,7 @@
+function[red_eltrim] = red_eltrim(m_eltrim,Cmdelta,CmTc,Tcs,Tc)
+%Calculates reduced elevator deflection based on measured elevator trim
+%angle in radians 'm_eltrim', elevator effectiveness
+%'Cmdelta', dimensionless thrust moment arm 'CmTc' given in tables C.1-C.3,
+%dimensionless standard thrust coefficient 'Tcs' (see p.10 of assignment &
+%appendix F) and the dimensionless coefficient 'Tc'
+red_eltrim = m_eltrim - 1/Cmdelta*CmTc*(Tcs-Tc);
