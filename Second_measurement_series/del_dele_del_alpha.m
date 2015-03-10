07f200f25e6f1c4c3096a7b8229a4602c3520310
Karl Martin Kajak <karlmartinkajak@wlan-145-94-204-223.wlan.tudelft.nl>
2015-03-10 16:02:51 +0100

    Finished elevator effectiveness calculation, also fixed other functions so that they wouldn't pass all I/O defined in the flowchart.

--- Second_measurement_series/del_dele_del_alpha.m
+++ Second_measurement_series/del_dele_del_alpha.m
@@ -1,6 +1,5 @@
+function[del_dele_del_alpha] = del_dele_del_alpha(Cmdelta,Cmalpha)
 %Calculate the derivative of the elevator deflection w.r.t the angle of
-%attack AND passes on elevator effectiveness Cmdelta
-function[del_dele_del_alpha,Cmdelta_o] = del_dele_del_alpha(Cmdelta,Cmalpha)
+%attack
 del_dele_del_alpha = -Cmalpha/Cmdelta;
-Cmdelta_o = Cmdelta;
 end
