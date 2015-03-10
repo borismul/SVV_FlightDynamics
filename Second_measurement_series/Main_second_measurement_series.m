86d57d65290d43dc3bdf791dbf8f8da24fcc1961
Nichsan <n.rasappu@gmail.com>
2015-03-10 19:17:53 +0100

    update

--- Second_measurement_series/Main_second_measurement_series.m
+++ Second_measurement_series/Main_second_measurement_series.m
@@ -2,8 +2,54 @@
 % writer: Nichsan
 
 
-% Define inputs:
-D_delta_e = ;
-D_x_cg = ;
-c_bar = ;
-rho
+%% Define inputs:
+
+% CONSTANTS
+p0 = 101325;            % Sea level air pressure                                    [Pa]
+lambda = ;              % ?                                                         [?]
+hp = ;                  % Pressure altitude                                         [m]
+T0 = 293;               % Sea level air temperature                                 [K]
+g0 = 9.81;              % Gravitational acceleration                                [m/s^2]
+R = 287;                % Dry air gas constant                                      [J/kg/K)]
+gamma = 1.4;            % Heat capacity ratio                                       [-]
+rho0 = 1.225;           % Sea level air density                                     [kg/m^3]
+Vcas = ;                % Calibrated airspeed                                       [m/s]
+Tm = ;                  % Measured temperature                                      [K]
+W = ;                   % Aircraft weight                                           [kg]
+S = ;                   % Aircraft wing surface                                     [m^2]
+Cm0 = ;                 % Pitching moment coefficient at zero angle of attack       [-]
+Cm_alpha = ;            % Pitching moment coefficient slope w.r.t. angle of attack	[1/rad]
+CN_alpha = ;            % Normal force coefficient slope w.r.t. angle of attack     [1/rad]
+Cm_delta_f = ;
+delta_f = ;
+Cm_Tc = ;
+Tc = ;
+Cm_lg = ;
+delta_e_s_e = ;
+Se = ;
+c_bar_e = ;
+Ch_delta = ;
+Cm_delta = ;
+x_cg = ;
+x_nfree = ;
+c_bar = ;               % Mean aerodynamic chord        [m]
+Ch_delta_t = ;
+delta_t_e = ;
+delta_t_e_0 = ;
+Ws = ;
+Cm_Tc = ;
+Tc_s = ;
+Tc = ;
+
+
+
+
+% MEASUREMENTS
+Ddelta_e = ;            % Change in elevator deflection [rad]
+Dx_cg = ;               % Change in c.g. position       [m]
+
+
+
+
+
+
