ac0f1aabe8a02d25784a71f688018e2a85457957
Nichsan <n.rasappu@gmail.com>
2015-03-10 17:41:02 +0100

    updates

---
+++ Second_measurement_series/Basic_parameters.m
@@ -0,0 +1,20 @@
+function[rho, CN] = basic(W, S, p0, lambda, hp, T0, g0, R)
+
+% function: determine the basic parameters
+% writer: Nichsan
+
+p = p0*(1+lambda*hp/T0)^(-g0/lambda/R); % Air pressure
+
+M = (2/(gamma-1)*((1+p0/p((1+(gamma-1)/(2*gamma)*rho0/p0*Vcas^2)^(gamma/(gamma-1))-1))^(1-1/gamma)-1))^0.5; % Mach number
+
+T = Tm/(1+(gamma-1)/2*M^2); % Air temperature
+
+a = (gamma*R*T)^0.5; % Speed of sound
+
+rho = p/(R*T); % Air density
+
+Vtas = M*a; % True airspeed
+
+CN = W/(0.5*rho*Vtas^2*S); % Normal force coefficient
+
+end
