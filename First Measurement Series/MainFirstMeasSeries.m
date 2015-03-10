3fb65f225e3eb4aac7fa02373f61b3dd20d4ceda
Boris Mulder <boris.mulder@live.nl>
2015-03-10 15:48:35 +0100

    Main inputs ingevoerd

--- First Measurement Series/MainFirstMeasSeries.m
+++ First Measurement Series/MainFirstMeasSeries.m
@@ -2,10 +2,25 @@ clc
 close all
 clear all
 
-hp = [1,2,3,4];
-M = [1,1.1,1,1];
-dT = [1,1,1,1];
-Mf1 = [1,1.1,1.2,1.3];
-Mf2 = [1,1.1,1.2,1.4];
+% Input vectors
+hp = [2000,2500,3000,8000];         % Pressure height                       (m)
+Mfl = [0.7,0.6,0.5,0.7];            % Fuel flow to left engine              (kg/s)
+Mfr = [0.65,0.5,0.55,0.6];          % Fuel flow to right engine             (kg/s)
+T_m = [280, 275, 270, 250];         % Measured temperature                  (K)
+VCAS = [180, 170, 175, 178];        % Calibrated airspeed                   (m/s)
+alpha = [0.09, 0.095 0.08, 0.04];  % Angle of attack                       (rad)
 
-thrust = ThrustExecution(hp,M,dT,Mf1,Mf2)
+% Input constants
+rampWeight = 50000;                 % Initial weight                        (kg)
+massFlow = 10;                      % Mass flow                             (kg/s)
+rho_0 = 1.225;                      % Density of the air at ground level    (kg/m^3)
+lambda = 0.005;                     % Temperature change                    (deg K/m)
+T_0 = 288;                          % Temperature at ground level           (deg K)
+g_0 = 9.81;                         % Gravity at ground level               (m/s^2)
+R = 288.05;                         % Universal gas constant air            (J* K^-1 * mol^-1)
+gamma = 1.4;                        % Ratio of specific heats               (-)
+p_0 = 10125;                        % pressure at ground level              (Pa)
+
+
+thrust = ThrustExecution(hp,M,dT,Mfl,Mfr);
+
