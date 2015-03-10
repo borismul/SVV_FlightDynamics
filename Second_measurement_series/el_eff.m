8a717dc1166637ee396f7e3571c18a47ed341ccb
Karl Martin Kajak <karlmartinkajak@wlan-145-94-204-223.wlan.tudelft.nl>
2015-03-10 17:11:03 +0100

    Changed elevator efficiency calculations to internally calculate lift coefficient and added inputs.

--- Second_measurement_series/el_eff.m
+++ Second_measurement_series/el_eff.m
@@ -1,5 +1,5 @@
-function[Cmdelta] = el_eff(del_dele,CN,del_xcg,cbar)
-%Calculates elevator effectiveness based on lift coefficient, change of
+function[Cmdelta] = el_eff(del_dele,del_xcg,cbar,rho,V,S,W)
+%Calculates elevator effectiveness based on lift coefficient (and associated variables), change of
 %required elevator deflection, change of the c.g., mean aerodynamic chord.
 %
 %Use:
@@ -7,5 +7,6 @@ function[Cmdelta] = el_eff(del_dele,CN,del_xcg,cbar)
 %Elevator_effectiveness = el_eff(change of elevator deflection in radians,
 %lift coefficient, change of center of gravity in meters, mean aerodynamic
 %chord in meters)
+CN = W/(0.5*rho*V^2*S);
 Cmdelta = -1/del_dele*CN*del_xcg/cbar;
 end
