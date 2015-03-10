e99d86e50d68f6a29aec37e2d07ff21c73b5bf4a
Nichsan <n.rasappu@gmail.com>
2015-03-10 14:57:20 +0100

    I added comments to long_stab

--- Second_measurement_series/long_stab.m
+++ Second_measurement_series/long_stab.m
@@ -1,5 +1,8 @@
 function[delta_e_alpha, Cm_delta] = long_stab(Cm_alpha)
 
-Cm_alpha = - Cm_delta*delta_e_alpha;
+% function: determine the moment coefficient slope w.r.t. angle of attack
+% writer: Nichsan
+
+Cm_alpha = - Cm_delta*delta_e_alpha; 
 
 end
