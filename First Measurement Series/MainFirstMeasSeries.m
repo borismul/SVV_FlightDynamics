15c710c7ec10ebbe0fc5fbd149f1fe96c2e3285f
Chrizzzl <christelprins@gmail.com>
2015-03-10 15:27:19 +0100

    AtmosphereParameters done

--- First Measurement Series/MainFirstMeasSeries.m
+++ First Measurement Series/MainFirstMeasSeries.m
@@ -11,3 +11,5 @@ Mf1 = [1,1.1,1.2,1.3];
 Mf2 = [1,1.1,1.2,1.4];
 
 thrust = ThrustExecution(hp,M,dT,Mf1,Mf2)
+
+[p,M,T,a,T_ISA] = AtmosphereParameters(p_0, rho_0, lamdba, h_p, T_0, T_m, g_0, R, gamma, V_cas);
