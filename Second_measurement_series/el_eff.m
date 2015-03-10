9a8b911e8f30c3c1eadc990822ea72d6eb8deeaf
Karl Martin Kajak <karlmartinkajak@wlan-145-94-204-223.wlan.tudelft.nl>
2015-03-10 17:18:35 +0100

    Added author, fixed error.

--- Second_measurement_series/el_eff.m
+++ Second_measurement_series/el_eff.m
@@ -7,6 +7,8 @@ function[Cmdelta] = el_eff(del_dele,del_xcg,cbar,rho,V,S,W)
 %Elevator_effectiveness = el_eff(change of elevator deflection in radians,
 %lift coefficient, change of center of gravity in meters, mean aerodynamic
 %chord in meters)
+%
+%Author: Karl
 CN = W/(0.5*rho*V^2*S);
 Cmdelta = -1/del_dele*CN*del_xcg/cbar;
 end
