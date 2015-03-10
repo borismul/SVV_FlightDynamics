f06614e9ec7080109da743d6de4a4c820d7ff822
Karl Martin Kajak <karlmartinkajak@wlan-145-94-204-223.wlan.tudelft.nl>
2015-03-10 17:04:02 +0100

    Deleted pointless code.

--- Second_measurement_series/del_dele_del_alpha.m
+++
@@ -1,5 +0,0 @@
-function[del_dele_del_alpha] = del_dele_del_alpha(Cmdelta,Cmalpha)
-%Calculate the derivative of the elevator deflection w.r.t the angle of
-%attack
-del_dele_del_alpha = -Cmalpha/Cmdelta;
-end
