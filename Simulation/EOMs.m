74c2e952387680671820e349d6946994874d5bad
Robert <rpfkoster@live.nl>
2015-03-12 16:35:08 +0100

    Finished EOM for the symmetric case
    indicated input, unit tested by input of ones

--- Simulation/EOMs.m
+++ Simulation/EOMs.m
@@ -4,37 +4,50 @@
 %
 %  by Robert
 
-function [ C1, C2, C3 ] = EOMs( muc, c, V0 )
+function [ C1, C2, C3 ] = EOMs( CX0, CXa, CXu, CXq, CXde, Cma, Cmadot, Cmu, Cmq, Cmde, ...
+                                CZ0, CZa, CZu, CZq, CZde, KY2, muc, c, V0 )
 %% Equations of Motion for the symmetric case
 % Input variables
 % see Cit_par.m
 
 C1 = zeros(4); % Define matrix size and fill with zeros
-% Overwrite cells in matrix C1 with nonzero values
-C1(1,1) =       - 2 * muc * c /(V0^2) ;
-C1(2,2) = (CZa - 2 * muc) * c / V0 ;
-C1(3,3) =                 - c / V0 ;
-C1(4,2) =          Cmadot * c / V0 ;
-C1(4,4) = - 2 * muc * KY2 *(c / V0)^2 ;
+% Overwrite cells in matrix C1 with nonzero values grouped by row
+    C1(1,1) = -       2 * muc * c /(V0^2) ;
+    
+    C1(2,2) = (CZa - 2 * muc) * c / V0    ;
+    
+    C1(3,3) = -                 c / V0    ;
+    
+    C1(4,2) =          Cmadot * c / V0    ;
+    C1(4,4) = - 2 * muc * KY2 *(c / V0)^2 ;
 
 C2 = zeros(4); % Define matrix size and fill with zeros
-% Overwrite cells in matrix C2 with nonzero values
-C2(1,:) = [1 1 1 1];
-C2(1,1) = CXu     / V0 ;
-C2(1,2) = CXa ;
-C2(1,3) = CZ0 ;
-C2(1,4) = CXq * c / Vo ;
-C2(2,:) = [1 1 1 1];
-C2(3,4) = 1;
-C2(4,:) = [1 1 0 1];
+% Overwrite cells in matrix C2 with nonzero values grouped by row
+    C2(1,1) = CXu     / V0 ;
+    C2(1,2) = CXa          ;
+    C2(1,3) = CZ0          ;
+    C2(1,4) = CXq * c / V0 ;
+    
+    C2(2,1) =   CZu                / V0 ;
+    C2(2,2) =   CZa                     ;
+    C2(2,3) = - CX0                     ;
+    C2(2,4) =  (CZq + 2 * muc) * c / V0 ;
+    
+    C2(3,4) = c / V0 ;
+    
+    C2(4,1) = Cmu     / V0 ;
+    C2(4,2) = Cma          ;
+    C2(4,4) = Cmq * c / V0 ;
 
 C3 = zeros(4,1); % Define matrix size and fill with zeros
-% Overwrite cells in matrix C3 with nonzero values
-C3(1,1) = CXde;
-C3(2,1) = CZde;
-C3(4,1) = Cmde;
+% Overwrite cells in matrix C3 with nonzero values grouped by row
+    C3(1,1) = CXde;
+    
+    C3(2,1) = CZde;
+    
+    C3(4,1) = Cmde;
 
-% Output variables
-%
+% Three output matrices
+% C1, C2 and C3
 end
 
