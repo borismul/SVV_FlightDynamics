f4ddc1336777745a2b08c6e5000f4a48bb24d543
Nichsan <n.rasappu@gmail.com>
2015-03-12 12:42:03 +0100

    update

--- Second_measurement_series/Main_second_measurement_series.m
+++ Second_measurement_series/Main_second_measurement_series.m
@@ -13,7 +13,6 @@ R = 287;                % Dry air gas constant                                  
 gamma = 1.4;            % Heat capacity ratio                                               [-]
 rho0 = 1.225;           % Sea level air density                                             [kg/m^3]
 
-
 % AIRCRAFT DEPENDENT CONSTANTS
 S = ;                   % Aircraft wing surface                                             [m^2]
 Se = ;                  % Elevator surface area                                             [m^2]
@@ -38,11 +37,15 @@ delta_e_alpha;          % Elevator deflection angle slope w.r.t. angle of attack
 CN_alpha = ;            % Normal force coefficient slope w.r.t. angle of attack             [1/rad]
 
 
-% Still undetermined; update later.
+
+
+% Still undetermined (only the first two are given in the tables of appendix C, but it is not clear whether we have to use these values or calculate from test
+% data; the rest must either be given in the flight test data or they have to be calculated from test data and thus the test data has to be studied first)
 Cm0 = ;                 % Pitching moment coefficient at zero angle of attack               [-]
+Cm_Tc = ;               % Dimensionless thrust moment arm                                   [-]
+
 Cm_delta_f = ;          % Pitching moment coefficient slope w.r.t flap deflection angle     [1/rad]
 delta_f = ;             % Flap deflection angle                                             [rad]
-Cm_Tc = ;               % Dimensionless thrust moment arm                                   [-]
 Tc = ;                  % Thrust coefficient                                                [-]
 Cm_lg = ;               % Pitching moment coefficient slope w.r.t landing gear deflection   [1/rad]
 delta_e_s_e = ;         % Elevator deflection angle slope w.r.t. stick deflection           [rad/m]
