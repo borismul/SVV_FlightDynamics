0fdd8d9bbaec475ef954a6da2821bb5b867cc497
Robert <rpfkoster@live.nl>
2015-03-12 11:29:40 +0100

    Added values to EOMs

--- Simulation/EOMs.m
+++ Simulation/EOMs.m
@@ -11,15 +11,19 @@ function [ C1, C2, C3 ] = EOMs( muc, c, V0 )
 
 C1 = zeros(4); % Define matrix size and fill with zeros
 % Overwrite cells in matrix C1 with nonzero values
-C1(1,1) = - 2 * muc * c / (V0^2) ;
+C1(1,1) =       - 2 * muc * c /(V0^2) ;
 C1(2,2) = (CZa - 2 * muc) * c / V0 ;
-C1(3,3) = - c / V0 ;
-C1(4,2) = Cmadot * c / V0 ;
-C1(4,4) = - 2 * muc * KY2 * (C / V0)^2;
+C1(3,3) =                 - c / V0 ;
+C1(4,2) =          Cmadot * c / V0 ;
+C1(4,4) = - 2 * muc * KY2 *(c / V0)^2 ;
 
 C2 = zeros(4); % Define matrix size and fill with zeros
 % Overwrite cells in matrix C2 with nonzero values
 C2(1,:) = [1 1 1 1];
+C2(1,1) = CXu     / V0 ;
+C2(1,2) = CXa ;
+C2(1,3) = CZ0 ;
+C2(1,4) = CXq * c / Vo ;
 C2(2,:) = [1 1 1 1];
 C2(3,4) = 1;
 C2(4,:) = [1 1 0 1];
