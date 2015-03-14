a0d4c26ba0133e6c03d1ec404f68fbb1452cc3b3
Nichsan <n.rasappu@gmail.com>
2015-03-14 17:00:57 +0100

    update

--- Second_measurement_series/Main_second_measurement_series.m
+++ Second_measurement_series/Main_second_measurement_series.m
@@ -8,18 +8,23 @@ clear all
 %% Define known input data
 
 % Atmospheric parameters
-p0 = 101325;        % Air pressure at sea level                             [Pa]
-lambda = -0.0065;   % Temperature gradient below Troposphere                [K/m]
-T0 = 288;           % Air temperature at sea level                          [K]
-g0 = 9.81;          % Gravitational acceleration                            [m/s^2]
-R = 288.05;         % Dry air gas constant                                  [J/kg/K)]
-gamma = 1.4;        % Heat capacity ratio                                   [-]
-rho0 = 1.225;       % Air density at sea level                              [kg/m^3]
+p0 = 101325;                            % Air pressure at sea level                             [Pa]
+lambda = -0.0065;                       % Temperature gradient below Troposphere                [K/m]
+T0 = 288;                               % Air temperature at sea level                          [K]
+g0 = 9.81;                              % Gravitational acceleration                            [m/s^2]
+R = 288.05;                             % Dry air gas constant                                  [J/kg/K)]
+gamma = 1.4;                            % Heat capacity ratio                                   [-]
+rho0 = 1.225;                           % Air density at sea level                              [kg/m^3]
 
 % Aircraft parameters
-S = 30.00;          % Aircraft wing surface                                 [m^2]
-cbar = 15.911;      % Mean aerodynamic chord                                [m]
-emptyWeight = 9170;	% Aircraft's empty weight                           	[lbs]
+S = 30.00;                              % Aircraft wing surface                                 [m^2]
+cbar = 15.911;                          % Mean aerodynamic chord                                [m]
+emptyWeight = 9170;                     % Aircraft's empty weight                           	[lbs]
+Cm0 = 0.0297;                           % Pitching moment coefficient at zero inputs            [-]
+Ws = 60500;                             % Aircraft's standard weight                            [N]
+
+% Data from first measurement series
+CN_alpha = ;                            % Normal force coefficient slope w.r.t. angle of attack [1/rad]
 
 %% Read and convert measured data
 
@@ -38,16 +43,17 @@ filename = 'Flight20303.xlsx'; % Name of the excel with the measured data
 [V_TAS] = VTAS(a,M);                                                                                                % True airspeed                                 [m/s]
 [C_L] = CL(W,rho,VTAS,S);                                                                                           % Lift coefficient                              [-]
 [Cmdelta] = el_eff(dele_1,dele_2,C_L,del_xcg,cbar);                                                                 % Elevator effectiveness                        [-]
-% Note: the del_xcg calculation program must still be made
+% Note: the 'del_xcg' calculation program must still be made
 
-[r_V_e] = red_elev_defl(Vtas,rho,rho0,Ws,W);                                                                        % Reduced equivalent airspeed                   [m/s]
+[r_V_e] = red_elev_eq(Vtas,rho,rho0,Ws,W);                                                                          % Reduced equivalent airspeed                   [m/s]
 [Cm_alpha] = long_stab(delta_e_alpha,Cm_delta);                                                                     % Longitudinal stability                        [-]
+% Note: 'delta_e_alpha' to be determined by plotting the graph with the measured data, this graph must be made
+
 [delta_e] = elev_defl(Cm_delta,Cm0,Cm_alpha,CN_alpha,W,rho,Vtas,S,Cm_delta_f,delta_f,Cm_Tc,Tc,Cm_lg);               % Elevator deflection                           [rad]
 [red_eltrim] = red_eltrim(m_eltrim,Cmdelta,CmTc,Tcs,Tc);                                                            % Reduced elevator deflection                   [rad]
 [Fstare] = red_el_cf(ddeledse,Se,Cbare,Vh,Chdelta,Cmdelta,xcg,xnfree,cbar,W,S,rho,V,Chdeltat,deltate,deltate0,Ws);	% Elevator control force component              [N]
 
 
-% AIRCRAFT INDEPENDENT PARAMETERS
 
 
 % AIRCRAFT DEPENDENT CONSTANTS
@@ -64,17 +70,17 @@ x_cg = ;                % Center of gravity position                            
 Dx_cg = ;               % Change in c.g. position                                           [m]
 
 % Determined from graphs made with flight test data; program for graph must still be made.
-delta_e_alpha;          % Elevator deflection angle slope w.r.t. angle of attack            [-]
+delta_e_alpha = ;          % Elevator deflection angle slope w.r.t. angle of attack            [-]
 
 % Determined from graphs made with flight test data; follows from measurement series 1.
-CN_alpha = ;            % Normal force coefficient slope w.r.t. angle of attack             [1/rad]
+
 
 
 
 
 % Still undetermined (only the first two are given in the tables of appendix C, but it is not clear whether we have to use these values or calculate from test
 % data; the rest must either be given in the flight test data or they have to be calculated from test data and thus the test data has to be studied first)
-Cm0 = ;                 % Pitching moment coefficient at zero angle of attack               [-]
+
 Cm_Tc = ;               % Dimensionless thrust moment arm                                   [-]
 
 Cm_delta_f = ;          % Pitching moment coefficient slope w.r.t flap deflection angle     [1/rad]
--- Second_measurement_series/red_elev_eq.m
+++ Second_measurement_series/red_elev_eq.m
@@ -1,10 +1,10 @@
-function[r_V_e] = red_elev_defl(Vtas,rho,rho0,Ws,W)
+function[r_V_e] = red_elev_eq(Vtas,rho,rho0,Ws,W)
 
 % function: determine the reduced equivalent airspeed
 % writer: Nichsan
 
 Ve = Vtas*(rho/rho0)^0.5; % Equivalent airspeed
 
-r_V_e = V_e*(Ws/W)^0.5; % Reduced equivalent airspeed
+r_V_e = Ve*(Ws/W)^0.5; % Reduced equivalent airspeed
 
 end
