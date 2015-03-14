8a7b3eeb8671e791aee6dd45641f602a64003e98
Chrizzzl <christelprins@gmail.com>
2015-03-14 17:37:40 +0100

    Changed the pressure, now have reasonable outcomes

--- First Measurement Series/LinearRegression.m
+++ First Measurement Series/LinearRegression.m
@@ -5,13 +5,11 @@ linearFunction = polyfit(C_L.^2,C_D,1);
 
 %Determine C_D0 and e
 slope =linearFunction(1);
-e = (1)/(pi*A*slope);
-C_D0 = linearFunction(2);
+e = (1)/(pi*A*slope)
+C_D0 = linearFunction(2)
 
 linearCLalpha = polyfit(alpha,C_L,1);
-CLalpha = linearCLalpha(1);
+CLalpha = linearCLalpha(1)
+CLa0 = linearCLalpha(2)
 
-%plot for verification
-% linearCL2 = polyval(linearFunction, C_D)
-% plot(C_D,linearCL2,C_D,C_L.^2)
 end
