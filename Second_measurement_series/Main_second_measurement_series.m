a79f57e53e602d5a136b4535a93fd98b942ef21c
Nichsan <n.rasappu@gmail.com>
2015-03-15 01:32:25 +0100

    update

--- Second_measurement_series/Main_second_measurement_series.m
+++ Second_measurement_series/Main_second_measurement_series.m
@@ -35,32 +35,32 @@ filename = 'Flight20303.xlsx'; % Name of the excel with the measured data
 [hp,Vc,alpha,delta_e,delta_e_t,Ffl,Ffr,Fuel_used,Tm,Fuel_start,Wempty]=Conversion_to_SI(hp,Vc,alpha,delta_e,delta_e_t,Ffl,Ffr,Fuel_used,Tm,Fuel_start,Wempty); % convert data to IS
 
 %% Summon data processing blocks
-[p,M,T,a,dT] = Atmospheric_parameters(p0,rho0,lambda,hp,T0,Tm,g0,R,gamma,Vc);                           % Air pressure, Mach number,                    [Pa],[-]
-                                                                                                        % Air temperature, speed of sound,              [K],[m/s]
-                                                                                                        % Difference of ISA w.r.t. standard temperature	[K]
-[W] = Aircraft_weight(Wempty,Fuel_start,Payload,Fuel_used);                                             % Aircraft weight                               [kg]
-[rho] = Air_density(p,R,T);                                                                             % Air density                                   [kg/m^3]
-[Vt] = True_airspeed(M,a);                                                                              % True airspeed                                 [m/s]
-[CN] = Normal_force_coefficient(W,rho,Vt,S);                                                            % Normal force coefficient                      [-]
-[Cm_delta] = Elevator_effectiveness(delta_e_1,delta_e_2,CN,Dxcg,cbar)                                    % Elevator effectiveness                        [-]
+[p,M,T,a,dT] = Atmospheric_parameters(p0,rho0,lambda,hp,T0,Tm,g0,R,gamma,Vc);                                   % Air pressure, Mach number,                        [Pa],[-]
+                                                                                                                % Air temperature, speed of sound,                  [K],[m/s]
+                                                                                                                % Difference of ISA w.r.t. standard temperature     [K]
+[W] = Aircraft_weight(Wempty,Fuel_start,Payload,Fuel_used);                                                     % Aircraft weight                                   [kg]
+[rho] = Air_density(p,R,T);                                                                                     % Air density                                       [kg/m^3]
+[Vt] = True_airspeed(M,a);                                                                                      % True airspeed                                     [m/s]
+[CN] = Normal_force_coefficient(W,rho,Vt,S);                                                                    % Normal force coefficient                          [-]
+[Cm_delta] = Elevator_effectiveness(delta_e_1,delta_e_2,CN,Dxcg,cbar)                                           % Elevator effectiveness                            [-]
 % Notes: the 'Dxcg' calculation program must still be made; 'Cmdelta' is an output of this program.
 
-[Ve_r] = Reduced_equivalent_airspeed(Vt,rho,rho0,Ws,W);                                                 % Reduced equivalent airspeed                   [m/s]
+[Ve_r] = Reduced_equivalent_airspeed(Vt,rho,rho0,Ws,W);                                                         % Reduced equivalent airspeed                       [m/s]
 
-[delta_e_alpha] = Elevator_deflection_wrt_angle_of_attack_slope(alpha,delta_e);                         % Elevator deflection slope w.r.t angle of attack [-]
-[Cm_alpha] = longitudinal_stability(delta_e_alpha,Cm_delta)                                                          % Longitudinal stability                        [-]
+[delta_e_alpha] = Elevator_deflection_wrt_angle_of_attack_slope(alpha,delta_e);                                 % Elevator deflection slope w.r.t angle of attack	[-]
+[Cm_alpha] = longitudinal_stability(delta_e_alpha,Cm_delta)                                                     % Longitudinal stability                            [-]
 % Note: 'Cm_alpha' is an output of this program.        
 
-[delta_e] = elevator_deflection(Cm_delta,Cm0,Cm_alpha,CN_alpha,W,rho,Vt,S,Cm_delta_f,delta_f,Cm_Tc,Tc,Cm_lg);	% Elevator deflection                           [rad]
+[delta_e] = elevator_deflection(Cm_delta,Cm0,Cm_alpha,CN_alpha,W,rho,Vt,S,Cm_delta_f,delta_f,Cm_Tc,Tc,Cm_lg);	% Elevator deflection                               [rad]
 % Note: 'Cm_delta_f' is nowhere to be found, but can't we leave it out,
 %        since 'delta_f' is always zero during the measurements?
 % Note: what exactly is Tc and how is it defined?
 % Note: isn't Cm_lg zero, since the landing gear is retracted?
 
-[delta_e_r] = reduced_elevator_deflection(delta_e,Cm_delta,Cm_Tc,Tc_s,Tc);                                                % Reduced elevator deflection                   [rad]
+[delta_e_r] = reduced_elevator_deflection(delta_e,Cm_delta,Cm_Tc,Tc_s,Tc);                                      % Reduced elevator deflection                       [rad]
 % Note: what exactly is Tc_s and how is it defined?
 
-[Fe_r] = Reduced_elevator_control_force(Fe,Ws,W);                                                                          % Reduced levator control force                 [N]
+[Fe_r] = Reduced_elevator_control_force(Fe,Ws,W);                                                               % Reduced levator control force                     [N]
 
 %% Plot outputs
 
