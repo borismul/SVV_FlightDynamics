fd14abc9ba8542e02938fcd51c6f880eb38ed095
Boris Mulder <boris.mulder@live.nl>
2015-03-16 11:49:09 +0100

    Unit test AirDensity

--- First Measurement Series/Unit_Tests.m
+++ First Measurement Series/Unit_Tests.m
@@ -4,7 +4,32 @@ clc
 close all
 clear all
 
+%Verification of the AirDensity function
+R = 287.05;                 %(J* K^-1 * mol^-1)
+T = 288.15;                 %(K)
+p = 90000;                  %(Pa)
 
+
+disp('------AirDensity------')
+
+% realistic value test
+rho = AirDensity(p,R,T);
+
+if rho < 1.0881 && rho> 1.088
+    disp('passed density realistic value test')
+else
+    disp('failed density realistic value test')
+end
+
+% R*T<0 test
+T=-1;
+
+try
+    rho = AirDensity(p,R,T);
+    disp('failed density R*T<0 test');
+catch
+    disp('passed density R*T<0 test');
+end
 
 %Verification of the WeigthAtTime function
 rampWeight = 5000;                   %[kg]
