246170492c5e3887b812a53968390d6ab7d0a612
Robert <rpfkoster@live.nl>
2015-03-16 10:27:36 +0100

    Created function to send to simulation
    Send aerodynamic properties to simulation

---
+++ First Measurement Series/SendToSimulation.m
@@ -0,0 +1,23 @@
+%% Create data file for simulation program
+%  Send data to the simulation folder, to be able to read the data in the
+%  simulation file
+%
+%  by Robert
+
+function [ ] = SendToSimulation( e, C_D0, CLalpha )
+%% Send aerodynamic properties to the Simulation folder
+%  Use the results from the first measurement series to calculate the
+%  aerodynamic properties.
+
+%  Send with name convention that is used in the Simulation folder
+e   = e       ;
+CD0 = C_D0    ;
+CLa = CLalpha ;
+
+%  Include datetime as timestamp to check for the last update
+timestamp = datetime('now') ;
+
+%  Save as matlab data file with name convention that is used in the
+%  Simulation
+save('../Simulation/FMS_aeroprop.mat','e','CD0','CLa','timestamp');
+end
