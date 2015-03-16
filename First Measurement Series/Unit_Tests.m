ff32693713d0699c13e56914dac4e786d8f91663
Chrizzzl <christelprins@gmail.com>
2015-03-16 10:35:55 +0100

    verificatioin file

--- First Measurement Series/Unit_Tests.m
+++ First Measurement Series/Unit_Tests.m
@@ -1,7 +1,18 @@
 %Unit Verification Test File
 %Created on 16-03-2015 by Christel and Boris
+clc
+close all
+clear all
 
 
+
+%Verification of the WeigthAtTime function
+rampWeight = 5000;                   %[kg]
+
+%fuelUsed = [-50,0,50,100,120,5500];  %[kg]
+fuelUsed = [0,20,50,100,125,6000]
+
+[W] = WeightAtTime(rampWeight,fuelUsed)
 
 
 
--- First Measurement Series/WeightAtTime.m
+++ First Measurement Series/WeightAtTime.m
@@ -1,4 +1,9 @@
 function [W] = WeightAtTime(rampWeight,fuelUsed)
-rampWeight = ones(size(fuelUsed))*rampWeight;
-W = rampWeight - fuelUsed;
+    rampWeight = ones(size(fuelUsed))*rampWeight;
+        
+    if sum(fuelUsed<0) ~= 0
+        error('Ammount of fuel increases, check the inputdata');
+    end
+
+    W = rampWeight - fuelUsed;
 end
