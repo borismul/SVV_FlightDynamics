3fa314732ab9e81983017ccb57ba5380fa2561f6
Karl Martin Kajak <karlmartinkajak@wlan-145-94-205-135.wlan.tudelft.nl>
2015-03-19 13:12:17 +0100

    Fixed trim curve slope file (redundant singularity check), added singularity check to thrust coefficient.

--- Second_measurement_series/Elevator_deflection_wrt_angle_of_attack_slope.m
+++ Second_measurement_series/Elevator_deflection_wrt_angle_of_attack_slope.m
@@ -3,7 +3,7 @@
 
 function[delta_e_alpha] = Elevator_deflection_wrt_angle_of_attack_slope(alpha,delta_e)
 
-delta_e_vs_alpha = polyfit(alpha,delta_e,1);
-delta_e_alpha = delta_e_vs_alpha(1);
+    delta_e_vs_alpha = polyfit(alpha,delta_e,1);
+    delta_e_alpha = delta_e_vs_alpha(1);
 
 end
--- Second_measurement_series/Thrust_coefficient.m
+++ Second_measurement_series/Thrust_coefficient.m
@@ -3,6 +3,11 @@
 
 function[Tc] = Thrust_coefficient(Thrust,rho,Vt,S)
 
+    % checking for singularities
+    if sum(rho.*Vt.^2.*S == 0) ~= 0
+        disp('Divide by 0 error!')
+    end
+
     Tc = 2./S.*Thrust./rho./(Vt.^2);
     
 end
